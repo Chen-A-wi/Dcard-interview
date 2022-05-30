31414171a38bf4abd8ffee5d6e8a49a5f6f6775c
Ben <tttw216@gmail.com>
2022-05-30 20:05:35 +0800

    [Fix] change flow to retrofit call

--- GithubSearch/app/src/main/java/com/example/searchdemo/data/repository/SearchRepository.kt
+++ GithubSearch/app/src/main/java/com/example/searchdemo/data/repository/SearchRepository.kt
@@ -2,14 +2,10 @@ package com.example.searchdemo.data.repository
 
 import com.example.searchdemo.api.SearchServices
 import com.example.searchdemo.data.Repositories
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.Response
+import retrofit2.Call
 
 class SearchRepository(private val services: SearchServices) {
-    fun getRepositories(keyword: String, page: Int): Flow<Response<Repositories>> {
-        return flow {
-            emit(services.getRepositories(keyword = keyword, page = page))
-        }
+    fun getRepositories(keyword: String, page: Int): Call<Repositories> {
+        return services.getRepositories(keyword = keyword, page = page)
     }
 }
