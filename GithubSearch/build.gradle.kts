9059d85d25edb9776e2d921cd4be2b0e53fcc702
BenChen <tttw216@gmail.com>
2023-05-11 11:25:43 +0800

    [Refactor] migrate project gradle to kotlin dsl

---
+++ GithubSearch/build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0")
+    }
+}
+
+plugins {
+    id ("com.android.application") version "7.2.1" apply false
+    id ("com.android.library") version "7.2.1" apply false
+    id ("org.jetbrains.kotlin.android") version "1.6.21" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
